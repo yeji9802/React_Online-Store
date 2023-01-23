d2000bfa2bc5d247cf879bcf60df7ed360469d5e
yeji <pyj9802@gmail.com>
2023-01-23 19:50:04 +0900

    style 추가

--- src/common/ProductItem.jsx
+++ src/common/ProductItem.jsx
@@ -2,18 +2,20 @@ import React from "react";
 
 const ProductItem = (props) => {
   const { category, items, inStockOnly } = props;
- 
-  const filterInStockOnly = inStockOnly ? items.filter((itme) => itme.stocked === inStockOnly) : items;
+
+  const filterInStockOnly = inStockOnly
+    ? items.filter((itme) => itme.stocked === inStockOnly)
+    : items;
   console.log(filterInStockOnly);
 
   return (
     <>
       <tr>
-        <td>{category}</td>
+        <td style={{ fontWeight: "bold" }}>{category}</td>
       </tr>
       {filterInStockOnly.map((item, idx) => (
         <tr key={idx}>
-          <td>{item.name}</td>
+          <td style={{ color: item.stocked ? "" : "red" }}>{item.name}</td>
           <td>{item.price}</td>
         </tr>
       ))}
